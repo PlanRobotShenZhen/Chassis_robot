302dbb01df38cfb59240442b81034f8e1a19b753
gss <1797001039@qq.com>
2023-11-27 16:51:22 +0800

    更新头文件

--- FLASH/FLASH_WR.h
+++ FLASH/FLASH_WR.h
@@ -1,9 +1,9 @@
 #ifndef __FLASH_WR_H
 #define __FLASH_WR_H
 
-uint16_t MyFLASH_ReadHalfWord(uint32_t Address);
+uint16_t MyFLASH_ReadWord(uint32_t Address);
 void MyFLASH_ErasePage(uint32_t Address);
-void MyFLASH_ProgramWord(uint32_t Address, uint32_t Data);
+void MyFLASH_ProgramWord(uint32_t Address, uint16_t Data);
 
 extern uint16_t Store_Data[];
 void Store_Init(void);
